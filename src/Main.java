0620f89805fda8e66ff1f3bb83e11433b2de372f
LK-NKOSI <222042761@edu.vut.ac.za>
2024-05-10 14:53:40 +0200

    Main change

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
        Scanner input = new Scanner(System.in);
-         System.out.print("Your name: ");
-         String name = input.next();
-        System.out.println("Hello world!");
+       System.out.print("Your name: ");
+       String name = input.next();
+       System.out.println("Hello name");
     }
 }
